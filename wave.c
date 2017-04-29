d06881c6bf0bb8ebce6e51e12d69cf18d57e82a7
Lovisa Lugnegard <lovisal@rackham4.uppmax.uu.se>
2017-04-29 14:53:08 +0200

    problem with memory allocation, solved by declarating arrays

--- wave.c
+++ wave.c
@@ -23,14 +23,15 @@ int main(int argc, char *argv[])
 
   MPI_Init(&argc,&argv);
 
-  int Nx,Ny,Nt,n_local_rows,n_local_columns,i,j,halo_size,u_size_local;
+  int Nx,Ny,Nt,n_local_rows,n_local_columns,i,j,halo_size,u_size_local,blocklength,stride,count,bonk2;
   double dt, dx, lambda_sq;
-  double *u,*u_local;
-  double *u_old,*u_old_local;
+  double *u; //,*u_local;
+  double *u_old, *u_old_local;
   double *u_new,*u_new_local;
   double begin,end;
 
-  Nx=128;
+
+  Nx=8; //ändrade till 8 för att kunna se vad som händer, är 128 i koden från uppgiften
   if(argc>1)
     Nx=atoi(argv[1]);
   Ny=Nx;
@@ -72,22 +73,27 @@ int main(int argc, char *argv[])
   u_size_local = (n_local_columns)*(n_local_rows);
   halo_size = n_local_rows + n_local_columns -1;
 
-  if(rank==0){
-    u = malloc(Nx*Ny*sizeof(double));
-    u_new = malloc(Nx*Ny*sizeof(double));}
+
   u_old_local = malloc((u_size_local)*sizeof(double));
   u_new_local = malloc((u_size_local)*sizeof(double));
-  u_local = malloc((u_size_local)*sizeof(double));
+
+  double u_local[u_size_local];
+
 
   /* Setup IC */
-  if(rank==0){
-    memset(u,0,Nx*Ny*sizeof(double));
-    memset(u_new,0,Nx*Ny*sizeof(double));}
   memset(u_local,0,u_size_local*sizeof(double));
   memset(u_old_local,0,u_size_local*sizeof(double));
   memset(u_new_local,0,u_size_local*sizeof(double));
+  stride=Nx;
+  count=Nx/sqnprocs;
+  blocklength=Nx/sqnprocs;
+
 
   if(rank==0){
+    u = malloc(Nx*Ny*sizeof(double));
+    u_new = malloc(Nx*Ny*sizeof(double));
+    memset(u,0,Nx*Ny*sizeof(double));
+    memset(u_new,0,Nx*Ny*sizeof(double));
     for(int i = 1; i < (Ny-1); ++i) {
       for(int j = 1; j < (Nx-1); ++j) {
         double x = j*dx;
@@ -98,6 +104,7 @@ int main(int argc, char *argv[])
 
         /* u1 */
         u_new[i*Nx+j] = initialize(x,y,dt);
+        printf("unew %d",u_new[i*Nx+j]);
       }
     }
     printf("\n IC complete \n");
@@ -108,90 +115,101 @@ int main(int argc, char *argv[])
     double max_error=0.0;
 #endif
 
-    MPI_Type_vector(Nx/sqnprocs,Nx/sqnprocs,Nx,MPI_DOUBLE,&strided);  
+    MPI_Type_vector(count,blocklength,stride,MPI_DOUBLE,&strided);  
     MPI_Type_commit(&strided);
 
-  //distribute all grid partitions
 
-    for(i=0; i<sqnprocs; i++) {
+    //distribute all grid partitions
+    for(i=0; i<sqnprocs; i++) { 
       for(j=0; j<sqnprocs; j++){
         MPI_Cart_rank(proc_grid,coords,&rank);
         //HÄR VAR VI NÄR VI SLUTADE!!!
-        MPI_Isend(&u_new[0],1,strided,(j+i*sqnprocs),1,proc_grid,&request[i*sqnprocs+j]);
-        // MPI_Isend(&dims,1,MPI_INT,(j+i*sqnprocs),1,proc_grid,&request[i*sqnprocs +j]);
-        printf("\n After Isend \n");
+
+        MPI_Isend(&u_new[(Nx*Nx/nprocs)*2*i+Nx/sqnprocs*j],1,strided,(j+i*sqnprocs),1,MPI_COMM_WORLD,&request[i*sqnprocs+j]);
       }
-    }
-    MPI_Waitall(nprocs-1,request,status);
-    printf("After waitall"); 
+    } 
   }
 
   MPI_Barrier(proc_grid);
   printf("\n after barrier \n");
-  //OBS ändra till Nx*Ny om de är olika stora
-  MPI_Recv(u_local,Nx*Nx/nprocs,MPI_DOUBLE,0,1,proc_grid,&status[rank]);
-  printf("\n After recieve \n");
 
 
-  /* Integrate */
+  MPI_Recv(u_local,Nx*Nx/nprocs,MPI_DOUBLE,0,1,MPI_COMM_WORLD,&status[rank]);
 
-  begin=timer();
-  for(int n=2; n<Nt; ++n) {
-    /* Swap ptrs */
-    double *tmp = u_old;
-    u_old = u;
-    u = u_new;
-    u_new = tmp;
+  MPI_Barrier(proc_grid);
 
-    /* Apply stencil */
-    for(int i = 1; i < (Ny-1); ++i) {
-      for(int j = 1; j < (Nx-1); ++j) {
+  MPI_Get_elements(&status[rank],MPI_DOUBLE,&bonk2);
+  printf("proc %d recv %d elements of type MPI_DOUBLE\n",rank,bonk2);
+  for (i=0;i<bonk2;i++)
+   
+    printf("proc: %d  %g\n",rank,u_local[i]);
 
-        u_new[i*Nx+j] = 2*u[i*Nx+j]-u_old[i*Nx+j]+lambda_sq*
-          (u[(i+1)*Nx+j] + u[(i-1)*Nx+j] + u[i*Nx+j+1] + u[i*Nx+j-1] -4*u[i*Nx+j]);
-      }
-    }
+  MPI_Finalize();
 
-#ifdef VERIFY
-    double error=0.0;
-    for(int i = 0; i < Ny; ++i) {
-      for(int j = 0; j < Nx; ++j) {
-        double e = fabs(u_new[i*Nx+j]-initialize(j*dx,i*dx,n*dt));
-        if(e>error)
-          error = e;
-      }
-    }
-    if(error > max_error)
-      max_error=error;
-#endif
+  //nu är alla u_local uppdaterade, nu behöver vi skicka halopunkter, här tror jag att det är en bra idé att använda sendrecv kolonn och radvis
 
-#ifdef WRITE_TO_FILE
-    save_solution(u_new,Ny,Nx,n);
-#endif
 
-  }
-  end=timer();
+  //bortkommenterat bara för att slippa hålla på med det just nu
 
-  printf("Time elapsed: %g s\n",(end-begin));
+  /* /\* Integrate *\/ */
 
-#ifdef VERIFY
-  printf("Maximum error: %g\n",max_error);
-#endif
+  /*   begin=timer(); */
+  /*   for(int n=2; n<Nt; ++n) { */
+  /*     /\* Swap ptrs *\/ */
+  /*     double *tmp = u_old; */
+  /*     u_old = u; */
+  /*     u = u_new; */
+  /*     u_new = tmp; */
 
-  free(u);
-  free(u_old);
-  free(u_new);
+  /*     /\* Apply stencil *\/ */
+  /*     for(int i = 1; i < (Ny-1); ++i) { */
+  /*       for(int j = 1; j < (Nx-1); ++j) { */
 
-  return 0;
-}
+  /*         u_new[i*Nx+j] = 2*u[i*Nx+j]-u_old[i*Nx+j]+lambda_sq* */
+  /*           (u[(i+1)*Nx+j] + u[(i-1)*Nx+j] + u[i*Nx+j+1] + u[i*Nx+j-1] -4*u[i*Nx+j]); */
+  /*       } */
+  /*     } */
 
-double timer()
-{
-  struct timeval tv;
-  gettimeofday(&tv, NULL);
-  double seconds = tv.tv_sec + (double)tv.tv_usec / 1000000;
-  return seconds;
-}
+  /* #ifdef VERIFY */
+  /*     double error=0.0; */
+  /*     for(int i = 0; i < Ny; ++i) { */
+  /*       for(int j = 0; j < Nx; ++j) { */
+  /*         double e = fabs(u_new[i*Nx+j]-initialize(j*dx,i*dx,n*dt)); */
+  /*         if(e>error) */
+  /*           error = e; */
+  /*       } */
+  /*     } */
+  /*     if(error > max_error) */
+  /*       max_error=error; */
+  /* #endif */
+
+  /* #ifdef WRITE_TO_FILE */
+  /*     save_solution(u_new,Ny,Nx,n); */
+  /* #endif */
+
+  /*   } */
+  /*   end=timer(); */
+
+  /*   printf("Time elapsed: %g s\n",(end-begin)); */
+
+  /* #ifdef VERIFY */
+  /*   printf("Maximum error: %g\n",max_error); */
+  /* #endif */
+
+  /*   free(u); */
+  /*   free(u_old); */
+  /*   free(u_new); */
+
+  /*   return 0; */
+} 
+
+/* double timer() */
+/* { */
+/*   struct timeval tv; */
+/*   gettimeofday(&tv, NULL); */
+/*   double seconds = tv.tv_sec + (double)tv.tv_usec / 1000000; */
+/*   return seconds; */
+/* } */
 
 double initialize(double x, double y, double t)
 {
